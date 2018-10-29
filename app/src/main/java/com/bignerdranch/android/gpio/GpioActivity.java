8f488cc0bf5cca88afd8c3a3923382e782ab0556
18301749585 <1251105538@qq.com>
2018-10-29 14:51:03 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/gpio/GpioActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.gpio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GpioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpio);
+    }
+}
